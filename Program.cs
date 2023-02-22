25c2192f2ec0c8d52a9b3434cd110d4173431c2f
bufaonanet <douglas.bufaonanet@outlook.com>
2023-02-22 17:49:34 -0300

    adicionando o swagger

--- Program.cs
+++ Program.cs
@@ -6,11 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddHttpClient<ICepService, CepService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
