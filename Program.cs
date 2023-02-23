9803c0065dc623e2d09d14dd259f569051656040
bufaonanet <douglas.bufaonanet@outlook.com>
2023-02-23 10:21:39 -0300

    adicionando healthchecks

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using BuscaCepApi.Services;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Net.Mime;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddHealthChecks();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,17 +18,35 @@ builder.Services.AddHttpClient<ICepService, CepService>();
 
 var app = builder.Build();
 
+app.UseHealthChecks("/status", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        var result = JsonSerializer.Serialize(
+            new
+            {
+                currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                status = report.Status.ToString(),
+            });
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
+});
+
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
+
+
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
